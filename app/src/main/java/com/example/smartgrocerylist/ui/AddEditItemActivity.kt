ca7af2ad6576de41e39fbdd2fb5add85a54bec2f
chaofan <chaofan.office@gmail.com>
2026-01-26 18:08:49 -0400

    Register activities

--- app/src/main/java/com/example/smartgrocerylist/ui/AddEditItemActivity.kt
+++ app/src/main/java/com/example/smartgrocerylist/ui/AddEditItemActivity.kt
@@ -1,4 +1,13 @@
 package com.example.smartgrocerylist.ui
 
-class AddEditItemActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smartgrocerylist.R
+
+class AddEditItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_item)
+    }
 }
--- app/src/main/java/com/example/smartgrocerylist/ui/SummaryActivity.kt
+++ app/src/main/java/com/example/smartgrocerylist/ui/SummaryActivity.kt
@@ -1,4 +1,12 @@
 package com.example.smartgrocerylist.ui
 
-class SummaryActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smartgrocerylist.R
+
+class SummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
 }
